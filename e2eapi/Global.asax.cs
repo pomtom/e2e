391f3367c529f6979e202e0b776bc7bdc451a51a
Pramod Lawate <pramodlovete36@gmail.com>
2018-06-23 14:51:17 +0100

    Added DbContext File to api project
    Added DbContext File to api project

--- e2eapi/Global.asax.cs
+++ e2eapi/Global.asax.cs
@@ -18,6 +18,7 @@ namespace e2eapi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Bootstrapper.Initialise();
         }
     }
 }
---
+++ e2eapi/Models/e2eDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace e2eapi.Models
+{
+    public class E2eDbContext : DbContext
+    {
+        public E2eDbContext()
+        {
+
+        }
+    }
+}
